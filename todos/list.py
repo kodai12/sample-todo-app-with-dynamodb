94a90607bb8aa1738b1103b8bbcc160216f79789
sakochik <sakochi.kodai1209@gmail.com>
2018-09-16 21:32:29 +0900

    add: python => decimalencoder & fetch all todos

---
+++ todos/list.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+from todos import decimalencoder
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+def list(event, context):
+  table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+  # fetch all todos from db
+  result = table.scan()
+
+  # create a response
+  response = {
+    'statusCode': 200,
+    'body': json.dumps(result['Item'], cls=decimalencoder.DecimalEncoder)
+  }
+
+  return response
